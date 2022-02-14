ba53bbe53e34f6af7045fd3fc55d7bcf11085c93
KennyHc <kennyhc@students.cs.ubc.ca>
2022-02-13 23:24:04 -0800

    added projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,6 +20,33 @@ import {
 } from '../../styles/GlobalComponents'
 import { projects } from '../../constants/constants'
 
-const Projects = () => <Section nopadding id="projects"></Section>
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
+)
 
 export default Projects
